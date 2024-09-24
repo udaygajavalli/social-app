0cf9d22c77f773138f75b6ca456be86139eec0ba
Uday Bhaskar Gajavalli <udaygajavalli@gmail.com>
2024-09-24 06:36:10 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import './App.css';
 import Sidebar from './components/Sidebar';
 import Stories from './components/Stories';
 import Feed from './components/Feed';
+import { CiSearch } from 'react-icons/ci';
 
 function App() {
   return (
     <div className="app-container">
       <Sidebar />
       <div className="main-content">
+        <div className="search">
+          <CiSearch />
+          <input
+            type="text"
+            placeholder="Search"
+            className="search-input"
+          ></input>
+        </div>
+
         <Stories />
         <Feed />
       </div>
